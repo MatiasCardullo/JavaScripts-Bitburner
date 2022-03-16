7943b3955a3ddeb5113f70c6a72203b7482c47ad
MatiasCardullo <supermaty97@gmail.com>
2022-03-16 16:15:57 -0300

    Update listMusic.bat

--- listMusic.bat
+++ listMusic.bat
@@ -1,5 +1,9 @@
 @echo off
-for /f "tokens=*" %%G in ('dir /b /s %~1\*.*') do (
+set "path=%~1"
+if not '%path:~-1%'=='\' set "path=%path%\"
+echo %path%
+pause
+for /f "tokens=*" %%G in ('dir /b /s %path%*.*') do (
 	if "%%~xG"==".wav" echo "%%G">>listMusicBitburner.txt
 	if "%%~xG"==".ogg" echo "%%G">>listMusicBitburner.txt
 	if "%%~xG"==".mp3" echo "%%G">>listMusicBitburner.txt
