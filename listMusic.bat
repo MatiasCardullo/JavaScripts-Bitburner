6c3f4d737b78da0e16934cbfa7e828b5719ea2d6
MatiasCardullo <supermaty97@gmail.com>
2022-03-16 16:04:12 -0300

    Fixed listMusic.bat

--- listMusic.bat
+++ listMusic.bat
@@ -1,10 +1,8 @@
 @echo off
 for /f "tokens=*" %%G in ('dir /b /s %~1\*.*') do (
-	set toList=false
-	if "%%~xG"==".wav" (set toList=true)
-	if "%%~xG"==".ogg" (set toList=true)
-	if "%%~xG"==".mp3" (set toList=true)
-	if %toList%==true (echo "%%G">>listMusicBitburner.txt)
+	if "%%~xG"==".wav" echo "%%G">>listMusicBitburner.txt
+	if "%%~xG"==".ogg" echo "%%G">>listMusicBitburner.txt
+	if "%%~xG"==".mp3" echo "%%G">>listMusicBitburner.txt
 )
-echo %cd%\listMusicBitburner.txt
+echo "%cd%\listMusicBitburner.txt"
 pause
